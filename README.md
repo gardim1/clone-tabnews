9228421b91913dd638fb31bbc6bce09da934b9f8
Vinicius Gardim <142423500+gardim1@users.noreply.github.com>
2024-10-15 20:48:18 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementacao do tabnews.com.br para o curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-function Home(){
-    return <h1>Vc faz o L</h1>
-    
+function Home() {
+  return <h1>Vc faz o L</h1>;
 }
 
 export default Home;
